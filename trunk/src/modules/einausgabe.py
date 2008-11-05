fb88c2b6a5e2d95c16c8499bdf9ec09e4dc65676
inf08 <inf08@9b9774a6-a656-0410-acd5-e12bd32c8b0b>
2008-11-05 08:37:09 +0000

    Pobi

--- trunk/src/modules/einausgabe.py
+++ trunk/src/modules/einausgabe.py
@@ -1,17 +1,17 @@
 import database
-db=database.Database()
+db=database.Database("")
 class Book():
     def create(self):
         db.query('insert into book')
         booknr=db.query('select MAX (nr)')
         return booknr
-    
+
     def create_type (self,isbn,author,title):
         if (db.check ("isbn",isbn) and db.check("text",author) and db.check("text",title)):
             suc=False
         else:
             db.query ('insert into type')
-            
+
     def edit(self,nr,isbn,author,title):
         if(db.check("nr",nr) and db.check("isbn",isbn) and db.check("text",author) and db.check("text",title)):
             if db.query('select nr from book where nr='+nr+''):
@@ -20,7 +20,7 @@ class Book():
             else:
                     suc=False
         return suc
-    
+
     def exist(self,booknr):
         if db.check("nr",booknr):
             if db.query('select nr from book where nr='+nr+''):
@@ -28,34 +28,34 @@ class Book():
             else:
                     suc=False
         return suc
-    
+
     def delete(self,booknr):
      if db.check("nr",booknr):
-            if db.query ('select nr from book where nr='+nr+''):
-                db.query ('delete booknr (nr)')
-                suc=True
-            else:
+         if db.query ('select nr from book where nr='+nr+''):
+             db.query ('delete booknr (nr)')
+             suc=True
+         else:
                     suc=False
-        return
-    
+         return suc
+
     def info(self,booknr):
-        info_dict={'select booknr, nr, isbn, author, title from type as t, from book as b where t.booknr=b.booknr' }
-        if db.query('select nr from book where nr='+nr''):
+        info_dict=('select booknr, nr, isbn, author, title from type as t, from book as b where t.booknr=b.booknr')
+        if db.query('select nr from book where nr='+str(nr))==True:
             suc=True
-            
+
         else:
             suc=False
-            
-        
-    
+
+
+
         return (info_dict)
-    
+
 class Pupil():
     def create(self):
         db.query('insert into pupil')
         booknr=db.query('select MAX (nr)')
         return pupilnr
-    
+
     def edit(self,nr,vorname,nachname,geburtsdatum):
         if(db.check("nr",nr) and db.check("text",vorname) and db.check("text",nachname) and db.check("text",geburtsdatum)):
             if db.query('select nr from book where nr='+nr+''):
@@ -82,3 +82,7 @@ class Ausleihe():
         return (booknrs)
     def handback(self,booknr):
         return
+
+#if "__main__"==__name__:
+ #   klasse=Book()
+  #  klasse.info(3)
