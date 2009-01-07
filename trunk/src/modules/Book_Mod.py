de360620d7d600a1b8837d5710d3d7a11e0e35df
inf08 <inf08@9b9774a6-a656-0410-acd5-e12bd32c8b0b>
2009-01-07 08:28:29 +0000

    Robart

--- trunk/src/modules/Book_Mod.py
+++ trunk/src/modules/Book_Mod.py
@@ -7,22 +7,30 @@ class Book():
         booknr=db.query('select max(nr) from book')
         return booknr[0][0]
 
-    def create_type (self,isbn,author,title):
-        if not(db.check ("isbn",isbn) and db.check("text",author) and db.check("text",title)):
+    def create_type (self,isbn,author,title,fachbereich,mittelstufe):
+        if not(db.check ("isbn",isbn) and db.check("text",author) and db.check("text",title) and db.check("text",fachbereich)and db.check ("nr",mittelstufe)):
             return False
         else:
             if db.query('select isbn from type where isbn='+isbn+''):
                 return False
             else:
-                db.query ('insert into type values (Null,"'+isbn+'", "'+author+'", "'+title+'")')
-                return db.query('SELECT nr from type WHERE (nr = "'+str(booknr)+'" AND b.type = t.nr)')
+                db.query ('insert into type values (Null,"'+isbn+'", "'+author+'", "'+title+'", "'+fachbereich+'", "'+mittelstufe+'")')
+                return db.query('SELECT nr from type WHERE isbn="'+isbn+'"')[0][0]
+        
 
-    def edit(self,nr,isbn,author,title):
-        if(db.check("nr",nr) and db.check("isbn",isbn) and db.check("text",author) and db.check("text",title)):
-            try:
-                db.query('update type set isbn="'+isbn+'" ,author="'+author+'" ,title="'+title+'" where nr='+nr+'')
+    def edit(self,nr,isbn,author,title,fachbereich,mittelstufe):
+        if(db.check ("isbn",isbn) and db.check("text",author) and db.check("text",title) and db.check("text",fachbereich)and db.check ("nr",mittelstufe)):
+            if self.type_exist(nr):
+                db.query('update type set isbn="'+isbn+'" ,author="'+author+'" ,title="'+title+'" ,fachbereich="'+fachbereich+'" ,mittelstufe="'+mittelstufe+'"where nr='+nr+'')
+                return True
+            else:
+                raise ValueError,"Invalid Number"
+
+    def type_exist(self,typenr):
+        if db.check("nr",typenr):
+            if db.query('select nr from type where nr='+typenr+''):
                 suc=True
-            except:
+            else:
                 suc=False
         return suc
 
@@ -33,7 +41,7 @@ class Book():
             else:
                 suc=False
         return suc
-    
+
     def book_type_connect(self,booknr,typenr):
         if db.check("nr",booknr) and db.check("nr",typenr):
             #liste=db.query('select nr from book where nr='+booknr+''), db.query('select nr from type where nr='+typenr+'')
@@ -41,7 +49,7 @@ class Book():
             return True
         else:
             return False
-    
+
     def delete(self,booknr):
         if db.check("nr",booknr):
             try:
@@ -52,32 +60,34 @@ class Book():
         else: return False
 
     def info(self,booknr):
-        info_dict=db.query('SELECT t.isbn, t.author, t.title FROM TYPE AS t, book AS b WHERE (b.nr = "'+str(booknr)+'" AND b.type = t.nr)')
+        info_dict=db.query('SELECT t.isbn, t.author, t.title t.fachbereich t.mittelstufe FROM TYPE AS t, book AS b WHERE (b.nr = "'+str(booknr)+'" AND b.type = t.nr)')
         #if db.query('select nr from book where nr='+nr+'')==True:
         #    suc=True
 
         #else:
 
         return (info_dict[0])
-    
+
     def type_info(self,nr):
         if db.check("nr",nr):
-            info_dict=db.query('Select isbn, author, title from type where nr="'+str(nr)+'"')
-            
+            info_dict=db.query('Select isbn, author, title, fachbereich, mittelstufe from type where nr="'+str(nr)+'"')
+
             try: return (info_dict[0])
             except: return False
-            
-        
+
+
 
 
 if "__main__"==__name__:
      buch=Book()
-     #print buch.create()                                                     #True
-     #print buch.create_type("9783596271207" , "DUUUL" , "Allgemeine Chemie" )#True
-     #print buch.exist("30")                                                  #True
-     #print buch.edit("3", "3499612453" , "DL" , "Allgemeine Chemie")         #True
-     #print buch.info("23")                                                   #True
-     #print buch.delete("9")                                                  #True
-     #print buch.type_info("3")                                               #True
-     #print buch.book_type_connect("7","3")                                   #True
-
+     #print buch.create()                                                                     
+     #print buch.create_type("9783507107229" , "Schroedel" , "Physik" , "Mathe" ,"1")         
+     #print buch.edit("16", "3499612453" , "Schroedel" , "Physik12/13" , "Physik" ,"0" )      
+     #print buch.type_exist("16")                                                             
+     #print buch.exist("99")                                                                  
+     #print buch.info("23")                                                                   
+     #print buch.delete("9")                                                                  
+     #print buch.type_info("16")                                                              
+     #for i in range (30):
+     #print buch.book_type_connect(str(i+1),"3")                                              
+     #print buch.book_type_connect ("33","16")
