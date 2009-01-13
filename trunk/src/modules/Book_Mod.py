602d005f66fe022eddd84616dc1cab68b2ece4c5
inf08 <inf08@9b9774a6-a656-0410-acd5-e12bd32c8b0b>
2009-01-13 11:39:46 +0000

    Robert

--- trunk/src/modules/Book_Mod.py
+++ trunk/src/modules/Book_Mod.py
@@ -8,15 +8,13 @@ class Book():
         return booknr[0][0]
 
     def create_type (self,isbn,author,title,fachbereich,mittelstufe):
-        if not(db.check ("isbn",isbn) and db.check("text",author) and db.check("text",title) and db.check("text",fachbereich)and db.check ("nr",mittelstufe)):
-            return False
-        else:
+        if (db.check ("isbn",isbn) and db.check("text",author) and db.check("text",title) and db.check("text",fachbereich)and db.check ("nr",mittelstufe)):
             if db.query('select isbn from type where isbn='+isbn+''):
-                return False
+                raise ValueError, "Isbn already exist"
             else:
                 db.query ('insert into type values (Null,"'+isbn+'", "'+author+'", "'+title+'", "'+fachbereich+'", "'+mittelstufe+'")')
                 return db.query('SELECT nr from type WHERE isbn="'+isbn+'"')[0][0]
-        
+
 
     def edit(self,nr,isbn,author,title,fachbereich,mittelstufe):
         if(db.check ("isbn",isbn) and db.check("text",author) and db.check("text",title) and db.check("text",fachbereich)and db.check ("nr",mittelstufe)):
@@ -29,43 +27,37 @@ class Book():
     def type_exist(self,typenr):
         if db.check("nr",typenr):
             if db.query('select nr from type where nr='+typenr+''):
-                suc=True
+                return True
             else:
-                suc=False
-        return suc
+                raise ValueError,"Number doesn't exist"
+
 
     def exist(self,booknr):
         if db.check("nr",booknr):
             if db.query('select nr from book where nr='+booknr+''):
-                suc=True
+                return True
             else:
-                suc=False
-        return suc
+                raise ValueError,"Number doesn't exist"
+
 
     def book_type_connect(self,booknr,typenr):
         if db.check("nr",booknr) and db.check("nr",typenr):
-            #liste=db.query('select nr from book where nr='+booknr+''), db.query('select nr from type where nr='+typenr+'')
             db.query('update book set type="'+typenr+'" where nr='+booknr+'')
             return True
         else:
-            return False
+            raise ValueError,"Invalid Booknumber"
 
     def delete(self,booknr):
         if db.check("nr",booknr):
             try:
-                if db.query ('Select nr from book where nr="'+booknr+'"') ==(): return False
+                if db.query ('Select nr from book where nr="'+booknr+'"') ==(): raise ValueError,"Number doesn't exist"
                 db.query ('delete from book where nr="'+booknr+'"')
                 return True
-            except: return False
-        else: return False
+            except: raise ValueError,"Error in Delete"
+
 
     def info(self,booknr):
         info_dict=db.query('SELECT t.isbn, t.author, t.title t.fachbereich t.mittelstufe FROM TYPE AS t, book AS b WHERE (b.nr = "'+str(booknr)+'" AND b.type = t.nr)')
-        #if db.query('select nr from book where nr='+nr+'')==True:
-        #    suc=True
-
-        #else:
-
         return (info_dict[0])
 
     def type_info(self,nr):
@@ -80,14 +72,14 @@ class Book():
 
 if "__main__"==__name__:
      buch=Book()
-     #print buch.create()                                                                     
-     #print buch.create_type("9783507107229" , "Schroedel" , "Physik" , "Mathe" ,"1")         
-     #print buch.edit("16", "3499612453" , "Schroedel" , "Physik12/13" , "Physik" ,"0" )      
-     #print buch.type_exist("16")                                                             
-     #print buch.exist("99")                                                                  
-     #print buch.info("23")                                                                   
-     #print buch.delete("9")                                                                  
-     #print buch.type_info("16")                                                              
+     #print buch.create()
+     #print buch.create_type("9783507107229" , "Schroedel" , "Physik" , "Mathe" ,"1")
+     #print buch.edit("16", "3499612453" , "Schroedel" , "Physik12/13" , "Physik" ,"0" )
+     #print buch.type_exist("16")
+     #print buch.exist("99")
+     #print buch.info("23")
+     #print buch.delete("9")
+     #print buch.type_info("16")
      #for i in range (30):
-     #print buch.book_type_connect(str(i+1),"3")                                              
+     #print buch.book_type_connect(str(i+1),"3")
      #print buch.book_type_connect ("33","16")
