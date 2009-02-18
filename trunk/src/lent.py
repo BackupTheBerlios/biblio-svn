89ca88e3ef68f9c0f948851b871469cf76e94bf1
rgiessmann <rgiessmann@9b9774a6-a656-0410-acd5-e12bd32c8b0b>
2009-02-18 08:20:33 +0000

    final release

--- trunk/src/lent.py
+++ trunk/src/lent.py
@@ -1,5 +1,5 @@
 # -*- coding: cp1252 -*-
-#Aus-/Rückgabe Frontend
+#Aus-/R&uuml;ckgabe Frontend
 #===============================================================================
 
 def content():
@@ -8,13 +8,13 @@ def content():
     htm=""
 
     htm+="""<table><tr>
-    <td><a href="./init.py?mn=lent&act=aus">Ausleihe</a></td>
-    <td><a href="./init.py?mn=lent&act=rueck">Rückgabe</a></td>
+    <td><a href="./start.py?mn=lent&act=aus">Ausleihe</a></td>
+    <td><a href="./start.py?mn=lent&act=rueck">R&uuml;ckgabe</a></td>
     </tr></table>"""
 
     if 'act' in form.keys():
         if form['act'].value == "aus":
-            htm=htm.replace('<a href="./init.py?mn=lent&act=aus">Ausleihe</a>',"...")
+            htm=htm.replace('<a href="./start.py?mn=lent&act=aus">Ausleihe</a>',"...")
 
             #Zuweisung der Variablen
             try: bn=form['bn'].value
@@ -30,13 +30,13 @@ def content():
             else:
                 htm+=aus()
         elif form['act'].value == "rueck":
-            htm=htm.replace('<a href="./init.py?mn=lent&act=rueck">Rückgabe</a>',"...")
+            htm=htm.replace('<a href="./start.py?mn=lent&act=rueck">R&uuml;ckgabe</a>',"...")
             if 'bn' in form.keys():
                 htm+=rueck(form['bn'].value)
             else:
                 htm+=rueck("")
         else:
-            htm+=html.message("Ungültiger Modus!","Leider gab es einen Fehler beim Aufruf...",0).rtn()
+            htm+=html.message("Ung&uuml;ltiger Modus!","Leider gab es einen Fehler beim Aufruf...",0).rtn()
     else:
         htm+=html.paragraph("Beginnen Sie mit einem Klick auf die jeweilige Aktion...").rtn()
 
@@ -62,7 +62,7 @@ def aus(lesernummer="",buchnummer="",mode=""):
                     htm+=html.paragraph('<div style="background-color:red">'+error.message+'</div>').rtn()
                     pass
             except ValueError:
-                htm+=html.paragraph('<div style="background-color:red">Bitte geben Sie gültige Nummern ein!</div>').rtn()
+                htm+=html.paragraph('<div style="background-color:red">Bitte geben Sie g&uuml;ltige Nummern ein!</div>').rtn()
 
         else:
             htm+=html.paragraph('<div style="background-color:red">Bitte Buch- <i>und</i> Lesernummer eingeben</div>').rtn()
@@ -74,13 +74,13 @@ def aus(lesernummer="",buchnummer="",mode=""):
     else:
         htm+='<body onload="document.fo.ln.focus();">'
 
-    htm+='''<form name="fo" action="./init.py" method="get">
+    htm+='''<form name="fo" action="./start.py" method="get">
             <input type="hidden" name="mn" value="lent" />
             <input type="hidden" name="act" value="aus" />
-            <p>Scannen oder wählen Sie bitte Leser- und Buchnummer aus:</p>
+            <p>Scannen oder w&auml;hlen Sie bitte Leser- und Buchnummer aus:</p>
             <p>
-            Lesernummer: <input type="text" name="ln" value="'''+ln+'''" maxlength="10" tabindex="1" onkeyup="if(document.fo.ln.value.length==10){document.fo.bn.focus()};" /><a href="./init.py?mn=pupil" target="_blank">Leser suchen...</a><br />
-            Buchnummer: <input type="text" name="bn" value="'''+bn+'''" maxlength="10" tabindex="2" onkeyup="if(document.fo.bn.value.length==10){document.fo.lend.focus()};" /><a href="./init.py?mn=books" target="_blank">Buch suchen...</a><br />
+            Lesernummer: <input type="text" name="ln" value="'''+ln+'''" maxlength="10" tabindex="1" onkeyup="if(document.fo.ln.value.length==10){document.fo.bn.focus()};" /><a href="./start.py?mn=pupil" target="_blank">Leser suchen...</a><br />
+            Buchnummer: <input type="text" name="bn" value="'''+bn+'''" maxlength="10" tabindex="2" onkeyup="if(document.fo.bn.value.length==10){document.fo.lend.focus()};" /><a href="./start.py?mn=books" target="_blank">Buch suchen...</a><br />
             <input type="submit" name="lend" value="Ausleihen." tabindex="3" />
             <input type="submit" name="save" value="Ausleihen und Lesernummer beibehalten..." tabindex="4" />
             </p>
@@ -101,21 +101,21 @@ def rueck(buchnummer=""):
             buchnummer = kurzlang.buch2kurz(buchnummer)
             try:
                 a.handback(buchnummer)
-                htm+=html.paragraph('<div style="background-color:green">Buch '+buchnummer+' wurde erfolgreich zurückgegeben.</div>').rtn()
+                htm+=html.paragraph('<div style="background-color:green">Buch '+buchnummer+' wurde erfolgreich zur&uuml;ckgegeben.</div>').rtn()
             except ValueError, error:
                 htm+=html.paragraph('<div style="background-color:red">'+error.message+'</div>').rtn()
         except ValueError:
-            htm+=html.paragraph('<div style="background-color:red">Bitte geben Sie eine gültige Nummer ein!</div>').rtn()
+            htm+=html.paragraph('<div style="background-color:red">Bitte geben Sie eine g&uuml;ltige Nummer ein!</div>').rtn()
             pass
 
     htm+='''<body onload="document.fo.bn.focus();">
-            <form name="fo" action="./init.py" method="get">
+            <form name="fo" action="./start.py" method="get">
             <input type="hidden" name="mn" value="lent" />
             <input type="hidden" name="act" value="rueck" />
-            <p><U>Scannen oder wählen Sie bitte die Buchnummer aus:</U></p>
+            <p><U>Scannen oder w&auml;hlen Sie bitte die Buchnummer aus:</U></p>
             <p>
-            <input type="text" name="bn" maxlength="10" tabindex="1" onkeyup="if(document.fo.bn.value.length==10){document.fo.mysubmit.focus()};" /><a href="./init.py?mn=books" target="_blank">Buch suchen...</a><br />
-            <input type="submit" name="mysubmit" value="Rückgabe" tabindex="2" />
+            <input type="text" name="bn" maxlength="10" tabindex="1" onkeyup="if(document.fo.bn.value.length==10){document.fo.mysubmit.focus()};" /><a href="./start.py?mn=books" target="_blank">Buch suchen...</a><br />
+            <input type="submit" name="mysubmit" value="R&uuml;ckgabe" tabindex="2" />
             </p>
             </form>'''
     return htm
